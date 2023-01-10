5c413e30b58d99b750897365b04e491598c2f23e
박동현 <donghyeondev@gmail.com>
2023-01-10 18:00:10 +0900

    채점사이트의 RE 확인 필요

---
+++ Hash/Main3-1.java
@@ -0,0 +1,34 @@
+/**
+ * @author : autocat
+ * @created : 2023-01-10
+**/
+import java.util.*;
+public class Main31{
+
+  public ArrayList<Integer>  solution(int n, int k, int[] arr){
+    
+    
+    return answer;
+    };
+
+
+  public static void main(String[] args){
+      Main31 main = new Main31();
+      Scanner scn = new Scanner(System.in);
+
+      int n = scn.nextInt();
+      int k = scn.nextInt();
+      int[] arr = new Integer[n];
+
+      ArrayList<Integer> resultList = main.solution(n, k, arr);
+      for(int i : resultList){
+        System.out.print(i +" ");
+      }
+    };
+
+     
+
+}
+
+
+
--- Hash/Main3.java
+++ Hash/Main3.java
@@ -3,12 +3,50 @@
  * @created : 2022-12-20
 **/
 import java.util.Scanner;
+import java.util.HashMap;
 public class Main3{
 
-  public String solution(String word){
-    
+  public int[] solution(int num, int days, int[] arr){
+      int[] answer = new int[days];
 
-      String answer = "";
+      HashMap<Integer, Integer> map = new HashMap<>();
+
+      int answerIndex = 0;
+      int count = 0;
+      int pl = 0;
+      int pr = days - 1;
+      int index = 0;
+
+      // 선작업으로 해시맵에 우측포인터 길이까지 넣어놓기
+      for(int i = 0; i < days-1; i++){ // 0, 1, 2
+        map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+      }
+
+      for(int i = days-1 ; i < num; i++){ // 3번 인덱스부터 시작
+        // answr에 HashMap갯수 넣기
+        //answer[answerIndex] = map.keySet().size();
+        //answerIndex++;
+        
+        //r 포인터 증가 
+        map.put(arr[i], map.getOrDefault(arr[i], 0) +1);
+        pr++;
+
+        answer[answerIndex] = map.keySet().size();
+        answerIndex++;
+
+        // l 포인터 증가 및 삭제
+        if(map.get(arr[pl]) == 1){
+          map.remove(arr[pl]);
+        } else {
+          map.put(arr[pl], map.get(arr[pl]) -1);
+        }
+        pl++;
+
+
+      }
+      
+
+
               
       return answer;
     };
@@ -22,6 +60,10 @@ public class Main3{
       int num = scn.nextInt();
       int days = scn.nextInt();
       int[] arr = new int[num];
+
+      for(int i = 0; i < num; i++){
+        arr[i] = scn.nextInt();
+      }
 
       int[] rtn = main.solution(num, days, arr);
       for(int a : rtn){
